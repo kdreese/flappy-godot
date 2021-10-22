dc918631c68c3072ca7432b38567b588367bf664
Terry Hearst <thearst3rd@gmail.com>
2021-10-21 22:04:04 -0400

    Implement collisions between bird <-> pipe

--- Objects/Pipe/Pipe.gd
+++ Objects/Pipe/Pipe.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+signal hit_pipe
+
 const SPEED = 100
 
 
@@ -12,3 +14,7 @@ func _process(delta: float) -> void:
 	position.x -= SPEED * delta
 	if position.x < -100:
 		queue_free()
+
+
+func _on_Pipe_body_entered(_body: Node) -> void:
+	emit_signal("hit_pipe")
--- States/Game/Game.gd
+++ States/Game/Game.gd
@@ -14,5 +14,11 @@ func _process(_delta: float) -> void:
 
 func _on_PipeSpawnTimer_timeout() -> void:
 	var pipe = pipe_scene.instance()
-	add_child(pipe)
 	pipe.position = Vector2(get_viewport_rect().size.x + 100, get_viewport_rect().size.y / 2)
+
+	pipe.connect("hit_pipe", self, "_on_Pipe_hit_pipe")
+	add_child(pipe)
+
+
+func _on_Pipe_hit_pipe() -> void:
+	$Bird.queue_free()
