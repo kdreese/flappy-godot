eaff7aacc3fb9f13ed6861d88b4563e69d58171b
Kyle Reese <reesekyle1998@gmail.com>
2021-10-28 22:51:24 -0400

    Added options menu with configurable buttons

--- States/Menu/Menu.gd
+++ States/Menu/Menu.gd
@@ -10,10 +10,15 @@ func _on_StartButton_pressed() -> void:
 	assert(not error)
 
 
+func _on_OptionsButton_pressed() -> void:
+	var error = get_tree().change_scene("res://States/OptionsMenu/OptionsMenu.tscn")
+	assert(not error)
+
+
 func _on_ExitButton_pressed() -> void:
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("quit"):
+	if event.is_action_pressed("escape"):
 		get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
